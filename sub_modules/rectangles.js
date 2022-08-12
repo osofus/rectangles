ccdc1d1f31d50efa635af5c1ec22efb5048f1f5b
Jobin Jose <jobin@osofus.com>
2022-08-12 09:41:16 -0700

    Ready to write unit tests

--- sub_modules/rectangles.js
+++ sub_modules/rectangles.js
@@ -1,11 +1,76 @@
 // properties of a rectangle
 class Rectangle {
-    constructor(width, height) {
-        this.width = width
-        this.height = height
+    constructor(origin, base, height) {
+        this.pointA = {
+            x: origin.x - base / 2,
+            y: origin.y - height / 2
+        }
+        this.pointB = {
+            x: origin.x + base / 2,
+            y: origin.y - height / 2
+        }
+        this.pointC = {
+            x: origin.x + base / 2,
+            y: origin.y + height / 2
+        }
+        this.pointD = {
+            x: origin.x - base / 2,
+            y: origin.y + height / 2
+        }
     }
-    get area() {
-        return this.width * this.height
+
+    contains(rectangle) {
+        if (
+            this.pointA.x < rectangle.pointA.x &&
+            this.pointB.x > rectangle.pointB.x &&
+            this.pointA.y < rectangle.pointA.y &&
+            this.pointD.y > rectangle.pointD.y
+        ) return true
+        else return false
+    }
+
+    separateFrom(rectangle) {
+        if (
+            this.pointA.x > rectangle.pointB.x ||
+            rectangle.pointA.x > this.pointB.x ||
+            this.pointA.y > rectangle.pointD.y ||
+            rectangle.pointA.y > this.pointD.y
+        ) return true
+        else return false
+    }
+
+    adjacentTo(rectangle) {
+        if (this.pointA.x == rectangle.pointB.x || rectangle.pointA.x == this.pointB.x) {
+            // vertical sides are touching
+            if (this.pointA.y == rectangle.pointA.y && this.pointD.y == rectangle.pointD.y) {
+                return "proper"
+            } else if (
+                this.pointA.y <= rectangle.pointA.y && this.pointD.y >= rectangle.pointD.y ||
+                rectangle.pointA.y <= this.pointA.y && rectangle.pointD.y >= this.pointD.y
+            ) {
+                return "sub-line"
+            } else if (
+                rectangle.pointA.y < this.pointA.y < rectangle.pointD.y ||
+                this.pointA.y < rectangle.pointA.y < this.pointD.y
+            ) {
+                return "partial"
+            } else return ""
+        } else if (this.pointA.y == rectangle.pointD.y || rectangle.pointA.y == this.pointD.y) {
+            // horizontal sides are touching
+            if (this.pointA.x == rectangle.pointA.x && this.pointB.x == rectangle.pointB.x) {
+                return "proper"
+            } else if (
+                this.pointA.x <= rectangle.pointA.x && this.pointB.x >= rectangle.pointB.x ||
+                rectangle.pointA.x <= this.pointA.x && rectangle.pointB.x >= this.pointB.x
+            ) {
+                return "sub-line"
+            } else if (
+                rectangle.pointA.x < this.pointA.x < rectangle.pointB.x ||
+                this.pointA.x < rectangle.pointA.x < this.pointB.x
+            ) {
+                return "partial"
+            } else return ""
+        } else return ""
     }
 }
 
@@ -16,14 +81,14 @@ module.exports = {
 
         // create rectangle 1
         server.post('/r1', function (req, res) {
-            let rectangle = new Rectangle(10, 15)
+            let rectangle = new Rectangle(req.body.origin, req.body.base, req.body.height)
             module.exports.r1 = rectangle
             res.send(rectangle)
         })
 
         // create rectangle 2
         server.post('/r2', function (req, res) {
-            let rectangle = new Rectangle(15, 20)
+            let rectangle = new Rectangle(req.body.origin, req.body.base, req.body.height)
             module.exports.r2 = rectangle
             res.send(rectangle)
         })
--- sub_modules/validators.js
+++ sub_modules/validators.js
@@ -6,9 +6,28 @@ module.exports = {
 
     routes: function (server) {
 
-        // check for interactions
+        // output rectangles
         server.get("/status", function (req, res) {
-            res.send({ areas: [rectangles.r1.area, rectangles.r2.area] })
+            res.send({ inputs: [rectangles.r1, rectangles.r2] })
+        })
+
+        // positional analysis
+        server.get("/stance", function (req, res) {
+            let outcome = ''
+            if (rectangles.r1 == undefined || rectangles.r2 == undefined) {
+                outcome = "Please define both rectangles first"
+            } else if (rectangles.r1.separateFrom(rectangles.r2))
+                outcome = "Rectangles are separate from each other"
+            else if (rectangles.r1.contains(rectangles.r2)) {
+                outcome = "r1 contains r2"
+            } else if (rectangles.r2.contains(rectangles.r1)) {
+                outcome = "r2 contains r1"
+            } else if (rectangles.r1.adjacentTo(rectangles.r2).length > 0) {
+                outcome = "Rectangles are adjacent (" + rectangles.r1.adjacentTo(rectangles.r2) + ")"
+            } else {
+                outcome = "Rectangles are intersecting"
+            }
+            res.send({ report: outcome })
         })
 
     }
